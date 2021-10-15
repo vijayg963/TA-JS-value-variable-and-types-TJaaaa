a4c7536e8fc2c27479946f99e15dcdfe85108200
vijayg963 <vijayg963@gmail.com>
2021-10-15 23:18:00 +0530

    Accessing-and-Assigning-Variable

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -7,16 +7,43 @@ let lastName = "Stark";
 
 // Alert the value of the firstName.
 // Alert the value of the lastName.
+
+alert(firstName);
+alert(lastName);
+
 // Assign a new value of `"John"` to firstName.
 // Assign a new value of `"Snow"` to lastName.
+
+firstName = "John";
+lastName = "Snow";
+
 // Alert the new value of `firstName` and `lastName` by concatenating them
+
+alert(firstName);
+alert(lastName);
+
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
+
+const fullName = firstName + " " + lastName;
 
 // Declare three variables `name,` `newUser` and `isAdmin`.
 // Assign your name as the value of `name`.
+
+let name = "name";
+
 // Assign the value of `name` to `newUser`.
+
+newUser = "newUser";
+
 // Set the value of `isAdmin` to `true`.
+
+let isAdmin = "true";
+
 // Alert the values of all the three variables.
+
+alert(name);
+alert(newUser);
+alert(isAdmin);
 
 // Assignment and re-assignment
 
@@ -25,9 +52,20 @@ var wiseMan = "Tyrion Lannister";
 // Follow instructions and do the following. In case of an error write the error in comment
 
 // Reassign the value of `wiseMan` to "Samwell Tarly"
+
+wiseMan = "Samwell Tarly";
+
 // Declare a variable `userName` with value "Lysa Arryn"
+
+var userName = "Lysa Arryn";
+
 // Declare a variable named `oddNumber` and assign a value `57`.
+
+var oddNumber = "57";
+
 // Reassign the value of `oddNumber` to 61
+
+oddNumber = "61";
 
 // Mathematical Operation
 
@@ -36,6 +74,25 @@ let amount = 4280;
 // Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
 
 // Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
+
+var reducedAmount;
+amount - 24;
+4256;
+
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
+
+var addedAmount;
+amount + 32;
+4312;
+
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
+
+var multipleAmount;
+amount * 7;
+29960;
+
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+
+var dividedAmount;
+amount / 57;
+75.08771929824562;
